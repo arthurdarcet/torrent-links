be6cce1bcdeeeda54a61bdf21c950cb7677cee08
Arthur Darcet <arthur.darcet@m4x.org>
2014-05-25 14:12:16 +0200

    Add a book category

--- upload.php
+++ upload.php
@@ -2,16 +2,12 @@
 
 $pass = 'secret';
 $base_dir = '/home/media/torrents/.watch/';
-$dirs = array(
-    'manual' => 'manual',
-    'show'   => 'show',
-    'movie'  => 'movie'
-);
+$dirs = array('manual', 'show', 'movie', 'book');
 
 if(!isset($_REQUEST['p']) || $_REQUEST['p'] != $pass || empty($_REQUEST['url']))
     exit;
 
-if(!isset($_REQUEST['cat']) || !array_key_exists($_REQUEST['cat'], $dirs))
+if(!isset($_REQUEST['cat']) || !in_array($_REQUEST['cat'], $dirs))
     $_REQUEST['cat'] = 'manual';
 
 $_REQUEST['url'] = urldecode($_REQUEST['url']);
@@ -20,7 +16,7 @@ if(substr($_REQUEST['url'],0,7) == 'magnet:')
 else
     $content = file_get_contents($_REQUEST['url']);
 
-$file = fopen($base_dir.$dirs[$_REQUEST['cat']].'/'.md5(time()).'.torrent', 'w');
+$file = fopen($base_dir.$_REQUEST['cat'].'/'.md5(time()).'.torrent', 'w');
 if(fwrite($file, $content) !== false)
     echo 'ok';
 fclose($file);
